1b2eb8e63bfc57004ce790af7cbbdb65ce350bc5
unknown <amanda@createlcom.com>
2021-04-25 10:26:59 +0700

    Fix model

--- main/backend/models/patient-model.js
+++ main/backend/models/patient-model.js
@@ -4,9 +4,12 @@ module.exports =(mongoose) => {
 
     const schema = new mongoose.Schema({
         name: {type: String, require: true},
-        username: {type: String, require: true},
         password: {type: String, require: true},
         email: {type: String, require: false},
+        RemindersInfo: [{
+            name: { type: String, required: true },
+            remindTime: {type: Date, required: true}
+        }]
     }, {
         strict: true,
         id: false,
